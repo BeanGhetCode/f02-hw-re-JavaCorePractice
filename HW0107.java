762a1bffbd699b97204f49e169642f397c1adc6e
Bean <144931571+BeanGhetCode@users.noreply.github.com>
2025-03-10 18:18:22 +0700

    feature(HW): Add HW0107

---
+++ HW0107.java
@@ -0,0 +1,7 @@
+package Introduce;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        System.out.println("Van toc: " + ((14/1.6)/((45.0/60)+(30.0/60/60)))+ " dam/h");
+    }
+}
