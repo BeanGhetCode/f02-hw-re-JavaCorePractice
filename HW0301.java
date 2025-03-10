3af36fe7e398c6900072541e9929e19d42018618
Bean <144931571+BeanGhetCode@users.noreply.github.com>
2025-03-11 00:42:26 +0700

    feature(HW): Add HW0301

---
+++ HW0301.java
@@ -0,0 +1,8 @@
+package OperatorAndConditions;
+
+public class HW01 {
+    public static void main(String[] args) {
+        String str = "Day la mot chuoi";
+        System.out.printf("Gia tri cua bien: %s", str);
+    }
+}
