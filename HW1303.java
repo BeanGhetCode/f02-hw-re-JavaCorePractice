c03049643be7b951afbc6b1b57cedcf931722507
Bean <144931571+BeanGhetCode@users.noreply.github.com>
2025-03-29 22:12:00 +0700

    refactor(HW): fix output HW1303

--- HW1303.java
+++ HW1303.java
@@ -6,12 +6,20 @@ import java.util.HashSet;
 public class HW1303 {
     public static void main(String[] args) {
         HashSet<Integer> result = new HashSet<Integer>();
-        int[] arr = {1,2,3,3,2,4,6,1,8};
+        int[] arr = {1,2,2,3,4,5,4};
 
         for(int i =0; i< arr.length; i++){
             result.add(arr[i]);
         }
-        System.out.println("Các số khác nhau: " + result);
+        System.out.print("Có " + result.size() + " số khác nhau: ");
+
+        // Dung StringBuilder loaij bo dau [] khi in ra
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int num:result){
+            stringBuilder.append(num).append(", ");
+        }
+        // Loai bo ", " o cuoi
+        System.out.print(stringBuilder.substring(0,stringBuilder.length()-2));
 
     }
 }
